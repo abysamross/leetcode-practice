64865c8c99e49320ee4e040719c2984155576199
Aby Sam Ross <abysamross@cse.iitb.ac.in>
2019-07-27 23:31:11 +0530

    Adding addingSmallStrings.cpp

---
+++ strings/addSmallStrings.cpp
@@ -0,0 +1,51 @@
+/*
+ * Add 2 small (lengthwise) enough strings.
+ */
+class Solution {
+    
+public:  
+    
+    void swap(string& s, int i, int j) {
+        
+        s[i] = s[i] ^ s[j];
+        s[j] = s[i] ^ s[j];
+        s[i] = s[i] ^ s[j];
+    }
+    
+    int getNum(string& s) {
+        
+        int num = 0;
+        for (auto c: s)
+            num = num*10 + c - '0';
+        
+        return num;
+    }
+    
+    string addStrings(string num1, string num2) {
+        
+        int n1 = 0;
+        int n2 = 0;
+        int sum = 0;
+        string ans;
+        
+        if (num1 == "" && num2 == "")
+            return "";
+        
+        n1 = getNum(num1);
+        n2 = getNum(num2);
+        sum = n1 + n2;
+        
+        if (!sum)
+            return "0";
+        
+        while (sum) {
+            ans.push_back('0' + sum%10);
+            sum /= 10;
+        }
+        
+        for (int i = 0, j = ans.size() - 1; i < j; ++i, --j)
+            swap(ans, i, j);
+        
+        return ans;
+    }
+};
