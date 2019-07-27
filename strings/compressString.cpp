0d2d2583b24bb5bb177d6fe4849948d5082be876
Aby Sam Ross <abysamross@cse.iitb.ac.in>
2019-07-27 16:54:05 +0530

    Adding 2 string problems
    - find the prob description on the top of the files

---
+++ strings/compressString.cpp
@@ -0,0 +1,46 @@
+/*
+ * Compress a string in place by replacing repeating chars in the string with 
+ * that char and the # times it repeat (if # > 1) and return the length of the
+ * compressed string.
+ */
+class Solution {
+
+public:
+    
+    void replaceCnt(vector<char>& chars, int rcnt, int& cInd) {
+        string scnt = "";
+        
+        while (rcnt) {
+
+            scnt.push_back('0' + rcnt%10);
+            rcnt /= 10;
+        }
+
+        for (int i = scnt.size() - 1; i >= 0; --i)
+            chars[++cInd] = scnt[i];
+    }
+    
+    int compress(vector<char>& chars) {
+        
+        int advInd;
+        int fInd = 0;
+        int cInd = 0;
+        
+        for (advInd = 1; advInd < chars.size(); advInd++) {
+            
+            if (chars[advInd] != chars[advInd - 1]) {
+                
+                if (advInd - fInd > 1)
+                    replaceCnt(chars, advInd - fInd, cInd);
+                
+                chars[++cInd] = chars[advInd];
+                fInd = advInd;
+            }
+        }
+       
+        if (advInd - fInd > 1)
+            replaceCnt(chars, advInd - fInd, cInd);
+        
+        return cInd+1;
+    }
+};
---
+++ strings/removeVowels.cpp
@@ -0,0 +1,31 @@
+/*
+ * Remove vowels from a string and compress the string
+ */
+class Solution {
+
+public:
+    string removeVowels(string S) {
+        int cInd = 0;
+        int advInd = 0;
+        
+        for (; advInd < S.size(); ++advInd) {
+            
+            switch (S[advInd]) {
+                case 'a':
+                case 'e':
+                case 'i':
+                case 'o':
+                case 'u':
+                    break;
+                default:
+                    S[cInd] = S[advInd];
+                    cInd++;
+                    break;
+            }
+        }
+        
+        S.erase(cInd, advInd - cInd);
+        return S;
+        
+    }
+};
