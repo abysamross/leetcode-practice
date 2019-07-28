2f22a79748c51e79b4922d0ed3e5d0fb326e085c
Aby Sam Ross <abysamross@cse.iitb.ac.in>
2019-07-28 12:35:50 +0530

    Adding reverseTokens.cpp
    - reverse each space separated words/tokens in a string

---
+++ strings/reverseTokens.cpp
@@ -0,0 +1,36 @@
+/*
+ * Reverse space separated tokens in a string
+ */
+class Solution {
+    
+public:
+    
+    string reverseWords(string s) {
+        
+        int sInd = 0;
+        int advInd = 0;
+        
+        for (; advInd <= s.size(); ++advInd) {
+            
+            if (advInd == s.size() || s[advInd] == ' ') {
+                
+                int st = sInd;
+                int en = advInd - 1;
+                // reverse the token
+                while (st < en) {
+
+                    s[st] = s[st] ^ s[en];
+                    s[en] = s[st] ^ s[en];
+                    s[st] = s[st] ^ s[en];
+
+                    st++;
+                    en--;
+                }
+                
+                sInd = advInd+1;
+            }
+        }
+        
+        return s;
+    }
+};
