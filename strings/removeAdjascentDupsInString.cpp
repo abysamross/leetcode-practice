5045f4eb3f0c52676217af241ee7b886c9bad859
Aby Sam Ross <abysamross@cse.iitb.ac.in>
2019-07-29 22:58:39 +0530

    Refining the logic of removeAdjascentDupsInString.cpp

--- strings/removeAdjascentDupsInString.cpp
+++ strings/removeAdjascentDupsInString.cpp
@@ -9,12 +9,14 @@ public:
     string removeDuplicates(string S) {
 		/*
 		 * Setup 4 indices:
-		 * st		= 	star of prefix w/o adjascent duplicates
-		 * b		= 	end of prefix w/o adjascent duplicates
+		 * st		= 	start of subseq w/o adjascent duplicates
+		 * b		= 	end of subseq w/o adjascent duplicates
 		 * f		= 	start of suffix yet to be scanned
 		 * en - 1 	= 	end of suffix yet to be scanned
 		 *
-		 * NOTE: S[b+1...f] is char that have been eliminated from string
+		 * NOTE: S[0...b] is the subseq that w/o adj dups
+		 * 		 s[b+1...f-1] is the dups that have been removed
+		 * 		 s[f...en-1] is the suffix yet to be scanned
 		 */
 		int st = -1;
         int en = S.size();
@@ -22,7 +24,7 @@ public:
         int f = 0;
         
         while (f < en - 2) {
-			// Append to prexif current char if it is not repeating.
+			// Append to subseq current char if it is not repeating.
             if (S[f] != S[f + 1])
                 S[++b] = S[f++];
 
@@ -30,43 +32,32 @@ public:
 				// if current char is repeating, skip the repeating char...
                 f += 2;
 				/*
-				 * and remove possible matches from the end of the already 
-				 * scanned prefix and the beginning of the yet to be scanned
+				 * and remove possible matches from the end of the 
+				 * subseq and the beginning of the yet to be scanned
 				 * suffix
 				 */
                 while ((b > st) && (S[b] == S[f])) {
                         --b; ++f;
                 }
 				/*
-				 * and in the process if we consumed the entire prefix
-				 * reset the st and b to f - 1, indicating that we need to 
-				 * consider only S[f...en-1] now on and start afresh. 
+				 * and in the process if we consumed the entire subseq 
+				 * we need to consider only S[f...en-1] now on and start afresh. 
 				 */
-                if (b <= st)
-                    st = b = f - 1;
             }
         }
-		// construct the solution string in place. 
-        int i = 0;
-		/*
-		 * if there is no prefix part, check only the last 2 chars.
-		 * else, move the prefix part S[st+1...b] to the begnning of S...
-		 */
-        while (st < b) 
-            S[i++] = S[++st];
-		/*
+		/* construct the solution string in place. 
 		 * check the last 2 chars as well, if they are different, move them to
 		 * the end of the solution
 		 */
         if (S[f] != S[f + 1])
             while (f < en)
-                S[i++] = S[f++];
+                S[++b] = S[f++];
 
 		/*
 		 * erase rest of string S and 
 		 * return S
 		 */
-        S.erase(i,  en - i);
+        S.erase(b + 1,  en - b);
         return S;
     }
 };
